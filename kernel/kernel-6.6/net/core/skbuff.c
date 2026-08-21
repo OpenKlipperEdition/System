cfbfa7c28d8f418bdb24e2500d2de16a375ae369
Dark98 <darkaidz98@gmail.com>
2026-08-21 00:46:53 +0000

    Kernel 6.6.136

--- kernel/kernel-6.6/net/core/skbuff.c
+++ kernel/kernel-6.6/net/core/skbuff.c
@@ -970,6 +970,7 @@ static bool skb_pp_recycle(struct sk_buff *skb, void *data, bool napi_safe)
 
 static void skb_kfree_head(void *head, unsigned int end_offset)
 {
+	/* Preserve the allocator/free pairing for cached small skb heads. */
 	if (end_offset == SKB_SMALL_HEAD_HEADROOM) {
 		kmem_cache_free(skb_small_head_cache, head);
 	} else {
